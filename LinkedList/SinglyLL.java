8d74737af066f7e6853ee8754b0a0c4da26dc7bd
Sinchana Gudal Siddeshappa <sinchana1209@gmail.com>
2026-02-18 23:00:06 +0530

    test commit

---
+++ LinkedList/SinglyLL.java
@@ -0,0 +1,18 @@
+
+class SinglyLL {
+    static class Node {
+        int data;
+        Node next;
+
+        Node(int data) {
+            this.data = data;
+            this.next = null;
+        }
+    }
+
+    public static void main(String[] args) {
+        SinglyLL sl = new SinglyLL();
+         
+
+    }
+}
